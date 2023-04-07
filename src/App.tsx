0e7d24e174a922ff4629b4c0e3f5ca193f640fa5
Artur Spatari <artur.spatari@gmail.com>
2023-04-07 14:24:14 +0300

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,13 @@ export default function App({}: Props) {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
