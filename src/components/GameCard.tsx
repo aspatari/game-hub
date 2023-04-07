2305110b6d60f153c2dafba730e387c1edb87d24
Artur Spatari <artur.spatari@gmail.com>
2023-04-07 17:52:12 +0300

    Cleanup game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,25 +5,22 @@ import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-  game: Game;
+    game: Game;
 }
 
 function GameCard({ game }: Props) {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-
-        <HStack justifyContent="space-between">
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-  );
+    return (
+        <Card>
+            <Image src={getCroppedImageUrl(game.background_image)} />
+            <CardBody>
+                <HStack justifyContent="space-between" marginBottom={3}>
+                    <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
+                <Heading fontSize="2xl">{game.name}</Heading>
+            </CardBody>
+        </Card>
+    );
 }
 
 export default GameCard;
