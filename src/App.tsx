91a6652f1ba4ee22ee70d2b252bc539e3c8a8067
Artur Spatari <artur.spatari@gmail.com>
2023-04-07 15:22:09 +0300

    Highlight the selecte genre

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,27 @@ import NavBar from "./components/NavBar";
 import { Genre } from "./hooks/useGenres";
 
 export default function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  console.log(selectedGenre);
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
-          <GenreList onSelect={(genre) => setSelectedGenre(genre)} />
-        </GridItem>
-      </Show>
-      <GridItem area="main">
-        <GameGrid selectedGenre={selectedGenre} />
-      </GridItem>
-    </Grid>
-  );
+    const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+    console.log(selectedGenre);
+    return (
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`,
+            }}
+            templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+        >
+            <GridItem area="nav">
+                <NavBar />
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside" paddingX={5}>
+                    <GenreList selectedGenre={selectedGenre} onSelect={(genre) => setSelectedGenre(genre)} />
+                </GridItem>
+            </Show>
+            <GridItem area="main">
+                <GameGrid selectedGenre={selectedGenre} />
+            </GridItem>
+        </Grid>
+    );
 }
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,42 +1,35 @@
-import {
-  Button,
-  HStack,
-  Image,
-  List,
-  ListItem,
-  Spinner,
-} from "@chakra-ui/react";
+import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-utl";
 interface Props {
-  onSelect: (genre: Genre) => void;
+    onSelect: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
-function GenreList({ onSelect }: Props) {
-  const { data, error, isLoadding } = useGenres();
-  if (error) return null;
-  if (isLoadding) return <Spinner />;
-  return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              onClick={() => onSelect(genre)}
-              fontSize="lg"
-              variant={"link"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
-  );
+function GenreList({ onSelect, selectedGenre }: Props) {
+    const { data, error, isLoadding } = useGenres();
+
+    if (error) return null;
+    if (isLoadding) return <Spinner />;
+
+    return (
+        <List>
+            {data.map((genre) => (
+                <ListItem key={genre.id} paddingY="5px">
+                    <HStack>
+                        <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                        <Button
+                            fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                            onClick={() => onSelect(genre)}
+                            fontSize="lg"
+                            variant={"link"}
+                        >
+                            {genre.name}
+                        </Button>
+                    </HStack>
+                </ListItem>
+            ))}
+        </List>
+    );
 }
 
 export default GenreList;
