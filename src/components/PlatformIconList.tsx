a57c4ee4514788a88fe2c1ccfed6f204e8bdda5a
Artur Spatari <artur.spatari@gmail.com>
2023-04-20 19:14:40 +0300

    Remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,42 +1,35 @@
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 
 import { IconType } from "react-icons";
 import { BsGlobe } from "react-icons/bs";
-import {
-  FaAndroid,
-  FaApple,
-  FaLinux,
-  FaPlaystation,
-  FaWindows,
-  FaXbox,
-} from "react-icons/fa";
+import { FaAndroid, FaApple, FaLinux, FaPlaystation, FaWindows, FaXbox } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 
 interface Props {
-  platforms: Platform[];
+    platforms: Platform[];
 }
 
 function PlatformIconList({ platforms }: Props) {
-  const iconMap: { [key: string]: IconType } = {
-    pc: FaWindows,
-    playstation: FaPlaystation,
-    xbox: FaXbox,
-    nintendo: SiNintendo,
-    mac: FaApple,
-    android: FaAndroid,
-    linux: FaLinux,
-    ios: MdPhoneIphone,
-    web: BsGlobe,
-  };
-  return (
-    <HStack marginY={1}>
-      {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
-      ))}
-    </HStack>
-  );
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        android: FaAndroid,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+    };
+    return (
+        <HStack marginY={1}>
+            {platforms.map((platform) => (
+                <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+            ))}
+        </HStack>
+    );
 }
 
 export default PlatformIconList;
