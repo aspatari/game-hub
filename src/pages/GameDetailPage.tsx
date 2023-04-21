f6343cd308151b604eb1e049eea50bec1ef65ba9
Artur Spatari <artur.spatari@gmail.com>
2023-04-21 22:02:20 +0300

    Improve layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
@@ -15,11 +15,18 @@ function GameDetailPage() {
     if (error) throw error;
     return (
         <>
-            <Heading>{game?.name}</Heading>
-            <ExpandableText>{game?.description_raw}</ExpandableText>
-            <GameAttributes game={game} />
-            <GameTrailers gameId={game.id} />
-            <GameScreenhots gameId={game.id} />
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+                <GridItem>
+                    <Heading>{game?.name}</Heading>
+                    <ExpandableText>{game?.description_raw}</ExpandableText>
+
+                    <GameAttributes game={game} />
+                </GridItem>
+                <GridItem>
+                    <GameTrailers gameId={game.id} />
+                    <GameScreenhots gameId={game.id} />
+                </GridItem>
+            </SimpleGrid>
         </>
     );
 }
